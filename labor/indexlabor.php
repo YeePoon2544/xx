7582b7b276b655467b3949fd68514ce37227fbcf
yeepoon <94604445+YeePoon2544@users.noreply.github.com>
2022-09-14 11:03:20 +0700

    14-09-65 (2)

--- labor/indexlabor.php
+++ labor/indexlabor.php
@@ -83,7 +83,7 @@ $data = mssql_query("SELECT * FROM labor");
     ?>
         <tr align="center">
             <td><?php echo $info['ID']; ?></td>
-            <td><?php echo $image ?></td>
+            <td><img id="showimg" src="uploads/<?php echo $image; ?>" style="height:120px; width:100px;"></td>
             <td><?php echo $details; ?></td>
             <td><?php echo $filename; ?></td>
             <td><a href='admin.php?Menu=3&Submenu=editlabor&ID=<?php echo $info['ID']; ?>'><button type="button" class="btn btn-warning">Edit</button></a></td>
@@ -91,4 +91,14 @@ $data = mssql_query("SELECT * FROM labor");
         </tr>
     <?php } ?>
 </table>
+<script>
+    var loadFile = function(event) {
+        var reader = new FileReader();
+        reader.onload = function() {
+            var output = document.getElementById('showimg');
+            output.src = reader.result;
+        };
+        reader.readAsDataURL(event.target.files[0]);
+    };
+</script>
 </html>
--- law/indexlaw.php
+++ law/indexlaw.php
@@ -86,7 +86,7 @@ $data = mssql_query("SELECT * FROM law");
     ?>
         <tr align="center">
             <td><?php echo $info['ID']; ?></td>
-            <td><?php echo $image; ?></td>
+            <td><img id="showimg" src="uploads/<?php echo $image; ?>" style="height:120px; width:100px;"></td>
             <td><?php echo $details; ?></td>
             <td><?php echo $filename; ?></td>
             <td><a href='admin.php?Menu=3&Submenu=editlaw&ID=<?php echo $info['ID']; ?>'><button type="button" class="btn btn-warning">Edit</button></a></td>
@@ -94,5 +94,15 @@ $data = mssql_query("SELECT * FROM law");
         </tr>
     <?php } ?>
 </table>
+<script>
+    var loadFile = function(event) {
+        var reader = new FileReader();
+        reader.onload = function() {
+            var output = document.getElementById('showimg');
+            output.src = reader.result;
+        };
+        reader.readAsDataURL(event.target.files[0]);
+    };
+</script>
 
 </html>
